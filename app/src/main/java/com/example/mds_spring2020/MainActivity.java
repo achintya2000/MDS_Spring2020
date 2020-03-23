77019becc1b797d94d358a1f487dfed819e96e11
Achintya Kumar <ask60@duke.edu>
2020-03-23 19:18:37 -0400

    simple addition calc

--- app/src/main/java/com/example/mds_spring2020/MainActivity.java
+++ app/src/main/java/com/example/mds_spring2020/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.mds_spring2020;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button addBtn = findViewById(R.id.addBtn);
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText editText1 = findViewById(R.id.editText1);
+                EditText editText2 = findViewById(R.id.editText2);
+                TextView result = findViewById(R.id.textView);
+
+                int num1 = Integer.parseInt(editText1.getText().toString());
+                int num2 = Integer.parseInt(editText2.getText().toString());
+
+                result.setText(num1 + num2 + "");
+            }
+        });
     }
 }
