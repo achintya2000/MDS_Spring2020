9712b946707ebb220ea7a019d4b68c9d752997b4
Achintya Kumar <ask60@duke.edu>
2020-03-24 15:55:01 -0400

    added google maps display in the uhf fragment

--- app/src/main/java/com/example/mds_spring2020/uhf.java
+++ app/src/main/java/com/example/mds_spring2020/uhf.java
@@ -17,9 +17,23 @@ import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.Toolbar;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import java.io.Console;
 
-public class uhf extends Fragment {
+public class uhf extends Fragment implements OnMapReadyCallback {
+
+    private GoogleMap map;
+    private MapView mMapView;
+    private View mView;
 
     public uhf() {
         // Required empty public constructor
@@ -29,12 +43,20 @@ public class uhf extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_uhf, container, false);
+        mView = inflater.inflate(R.layout.fragment_uhf, container, false);
+        return mView;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        mMapView = (MapView) mView.findViewById(R.id.mapView);
+        if (mMapView != null) {
+            mMapView.onCreate(null);
+            mMapView.onResume();
+            mMapView.getMapAsync(this);
+        }
 
     }
 
@@ -49,5 +71,11 @@ public class uhf extends Fragment {
         super.onPause();
         ((AppCompatActivity) getActivity()).getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.BLACK));
 
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        MapsInitializer.initialize(getContext());
+        map = googleMap;
     }
 }
